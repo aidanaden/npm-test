d10310f8533acd198d5fae4b177e615d8fc65e29
aidanaden <aidanaden@hotmail.com>
2022-11-22 16:11:09 +0800

    feat: update api

--- src/routes/api/notes.ts
+++ src/routes/api/notes.ts
@@ -2,16 +2,16 @@ import { prisma } from "~/server/db/client";
 import { json, type APIEvent } from "solid-start";
 
 export async function GET() {
-  return json(await prisma.notes.findMany());
+  const notes = await prisma.notes.findMany();
+  return json(notes);
 }
 
 export async function POST(event: APIEvent) {
   const { text } = await event.request.json();
-  return json(
-    await prisma.notes.create({
-      data: {
-        text,
-      },
-    })
-  );
+  const note = await prisma.notes.create({
+    data: {
+      text,
+    },
+  });
+  return json(note);
 }
