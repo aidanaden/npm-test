93d12a0df3b04b426ed84556b81da7b0c1e54503
aidanaden <aidanaden@hotmail.com>
2022-11-22 16:05:17 +0800

    feat: log res

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,10 @@
-import { type ParentComponent, Switch, Match, createResource } from "solid-js";
+import {
+  type ParentComponent,
+  Switch,
+  Match,
+  createResource,
+  createEffect,
+} from "solid-js";
 import { Title } from "solid-start";
 
 const Home: ParentComponent = () => {
@@ -6,19 +12,23 @@ const Home: ParentComponent = () => {
     fetch("/api/notes").then((res) => res.json())
   );
 
+  createEffect(() => {
+    console.log({ loading: res.loading, res: res() });
+  });
+
   return (
     <>
       <Title>Home</Title>
       <div>
-        <Switch 
+        <Switch
           fallback={
+            <div class="font-bold text-2xl text-gray-500">Loading...</div>
+          }
+        >
+          <Match when={!res.loading}>
             <pre class="font-bold text-2xl text-gray-500">
               {JSON.stringify(res(), null, 2)}
             </pre>
-          }
-        >
-          <Match when={res.loading}>
-            <div class="font-bold text-2xl text-gray-500">Loading...</div>
           </Match>
         </Switch>
       </div>
